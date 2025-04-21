adfc310d00fe215aa4f8c80d87337a511202156e
Michael-SvCruz <sv_yaco@hotmail.com>
2025-04-21 09:50:39 -0300

    atualizando entrypoint - incluindo .env

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 
 # Permissão no keyfile
 chmod 400 /etc/mongo-keyfile
+echo "🔐 Permissão no keyfile ajustada"
 
 # Inicia o mongod temporariamente sem auth
 mongod --replSet rs0 --bind_ip_all --keyFile /etc/mongo-keyfile --fork --logpath /var/log/mongod.log
+echo "🚀 Iniciando mongod temporário para configuração inicial"
 
 # Espera o mongod subir
 sleep 5
@@ -17,6 +19,7 @@ rs.initiate({
   members: [{ _id: 0, host: "mongodb:27017" }]
 })
 EOF
+echo "🧠 Replica set iniciado"
 
 # Espera o PRIMARY
 sleep 5
@@ -25,17 +28,20 @@ sleep 5
 mongosh <<EOF
 use admin
 db.createUser({
-  user: "admin",
-  pwd: "senha123",
+  user: "$MONGO_INITDB_ROOT_USERNAME",
+  pwd: "$MONGO_INITDB_ROOT_PASSWORD",
   roles: [ { role: "root", db: "admin" } ]
 })
 EOF
+echo "👤 Usuário root criado"
 
 # Para o mongod temporário
-mongosh -u admin -p senha123 --authenticationDatabase admin --eval "db.shutdownServer()"
+mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --eval "db.shutdownServer()"
+echo "🛑 Parando mongod temporário"
 
 # Espera sair do ar
 sleep 3
 
 # Inicia o mongod definitivo com auth e replicaset
 exec mongod --replSet rs0 --bind_ip_all --auth --keyFile /etc/mongo-keyfile
+echo "✅ MongoDB pronto com autenticação e replica set"
